dfc746bb9e9e216c57f02e6c844c69a74336e3c9
Raphael2790 <rssshev@gmail.com>
2019-09-20 05:14:28 -0300

    Iniciado os exercicios 20

--- challenge-20/challenge-20.js
+++ challenge-20/challenge-20.js
@@ -1,3 +1,5 @@
+(function (win , doc) {
+    'use strict';
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
@@ -14,43 +16,54 @@ resultado à uma variável chamada `username`. Se o usuário não digitar um
 nome, `username` deve receber "Desconhecido".
 Com a resposta, mostre um alert com a mensagem "Bem vindo [USERNAME]!"
 */
-// ?
+
+var username = win.prompt('Qual seu nome?')
+
+if (username) {
+win.alert(`Bem vindo ${username}`)
+} else { win.alert('Bem vindo Desconhecido')}
 
 /*
 Agora, pergunte ao usuário "Qual o seu e-mail?", atribuindo o resultado à
 uma variável chamada `email`.
 */
-// ?
+var email = prompt('Qual seu e-mail?')
 
 /*
 - Selecione o input de "Nome", atribuindo-o à uma variável chamada
 `$inputUsername`.
 */
-// ?
+
+var $inputUsername = doc.querySelector('#nome')
 
 /*
 - Selecione o input de "Email", atribuindo-o à uma variável chamada
 `$inputEmail`.
 */
-// ?
+
+var $inputEmail = doc.querySelector('#email')
 
 /*
 - Selecione o campo de "Mensagem", atribuindo-o à uma variável chamada
 `$message`.
 */
-// ?
+
+var $message = doc.querySelector('textarea')
 
 /*
 - Selecione o botão de envio do formulário, atribuindo-o à uma variável
 chamada `$button`.
 */
-// ?
+
+var $button = doc.querySelector('#botao')
 
 /*
 Preencha os campos de "Nome" e "Email" que estão no documento com os valores
 entrados pelo usuário.
 */
-// ?
+
+$inputUsername.value = username
+$inputEmail.value = email
 
 /*
 Adicione um listener de evento de click ao botão que faça o seguinte:
@@ -76,7 +89,12 @@ Se for confirmado, mostre um alerta com a mensagem:
 Caso contrário, mostre um alerta com a mensagem:
     - "Não enviado."
 */
-// ?
+
+$button.addEventListener('click', Enviar )
+
+function Enviar() {
+    alert('Enviado')
+}
 
 /*
 Crie uma função chamada `isValidEmail`, que será usada na validação do
@@ -104,4 +122,4 @@ Alguns e-mails inválidos:
     - "rita-marica@titica.a.b"
     - "agua_@evida.br.com"
 */
-// ?
+})(window, document)
