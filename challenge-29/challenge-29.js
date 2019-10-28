570e31676bc66dcb23b2672d7b3fe3294a2cf566
Raphael2790 <rssshev@gmail.com>
2019-10-28 05:17:06 -0300

    Exercicios 29 Iniciado

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(() => {
   'use strict';
 
   /*
@@ -35,5 +35,12 @@
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
   que será nomeado de "app".
   */
+  function app() {
+    var $nomeLoja = document.querySelector('h1');
+    var telefone = document.querySelector('h2');
+    var ajax = new XMLHttpRequest()
 
-})();
+  }
+
+
+})(window.DOM);
